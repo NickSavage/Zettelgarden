37a31c49a201e50fb778a8646c1b83e34d64ceb4
Nick Savage <nick@fortuna>
2023-09-24 20:04:19 -0400

    Continuing to build the product

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from urllib.parse import unquote
 import sqlite3
 import re
 
@@ -33,47 +34,64 @@ cur.execute(
             """
     )
 conn.commit()
+cur.close()
 
+
+def sort_ids(id):
+    # Use regular expressions to split the id into numeric and non-numeric parts
+    parts = re.split(r'(\D+)', id)
+    # Convert numeric parts to integers
+    parts = [part.zfill(5) if part.isdigit() else part for part in parts]
+    return parts
+
+def get_backlinks(card_id):
+    cur = conn.cursor()
+    cur.execute("SELECT backlinks.source_id, cards.title FROM backlinks JOIN cards ON backlinks.source_id = cards.id WHERE target_id = ?;", (card_id,))
+    backlinks = [{"target_id": row[0], "title": row[1]} for row in cur.fetchall()]
+    cur.close()
+    return backlinks
 
 def update_backlinks(id, backlinks):
+    print(id, backlinks)
+    cur = conn.cursor()
     # Delete existing backlinks for the card
     cur.execute("DELETE FROM backlinks WHERE source_id = ?;", (id,))
     # Insert new backlinks
     for target_id in backlinks:
         cur.execute("INSERT INTO backlinks (source_id, target_id) VALUES (?, ?);", (id, target_id))
         conn.commit()
+    cur.close()
 
 # Utility to extract backlinks from text
 def extract_backlinks(text):
-        # find all patterns like [A.1]
-        return re.findall(r'\[(\w+\.\d+)\]', text)
+    # find all patterns like [anything here]
+    return re.findall(r'\[([^\]]+)\]', text)
 
 # Routes
 
 @app.route('/cards', methods=['GET'])
 def get_cards():
+    cur = conn.cursor()
     cur.execute("SELECT id, title, body FROM cards;")
     cards = cur.fetchall()
     results = []
     for x in cards:
         id = x[0]
-        cur.execute("SELECT target_id FROM backlinks WHERE source_id = ?;", (id,))
-        backlinks = [row[0] for row in cur.fetchall()]
         card = {
             "id": x[0],
             "title": x[1],
             "body": x[2],
-            "backlinks": backlinks
-            
+            "backlinks": get_backlinks(x[0])
         }
         results.append(card)
-    results = sorted(results, key=lambda x: x["id"])
-    print(results)
+    results = sorted(results, key=lambda x: sort_ids(x["id"]))
+    cur.close()
     return jsonify(results)
 
 
 @app.route('/cards', methods=['POST'])
 def create_card():
+    cur = conn.cursor()
     id = request.json.get("id")
     title = request.json.get("title")
     body = request.json.get("body")
@@ -88,29 +106,33 @@ def create_card():
     # Update backlinks
     backlinks = extract_backlinks(body)
     update_backlinks(id, backlinks)
+    cur.close()
     
     return jsonify({"id": id, "title": title, "body": body})
                                                         
 
-@app.route('/cards/<id>', methods=['GET'])
+@app.route('/cards/<path:id>', methods=['GET'])
 def get_card(id):
+    id = unquote(id)
+    print(id)
+    cur = conn.cursor()
     cur.execute("SELECT * FROM cards WHERE id = ?;", (id,))
     card = cur.fetchone()
     if card:
-        cur.execute("SELECT target_id FROM backlinks WHERE source_id = ?;", (id,))
-        backlinks = [row[0] for row in cur.fetchall()]
         card = {
             "id": card[0],
             "title": card[1],
             "body": card[2],
-            "backlinks": backlinks
+            "backlinks": get_backlinks(card[0])
             
         }
-        return jsonify(card)
+    cur.close()
+    return jsonify(card)
 
 
-@app.route('/cards/<id>', methods=['PUT'])
+@app.route('/cards/<path:id>', methods=['PUT'])
 def update_card(id):
+    cur = conn.cursor()
     title = request.json.get("title")
     body = request.json.get("body")
     
@@ -122,6 +144,7 @@ def update_card(id):
     backlinks = extract_backlinks(body)
     update_backlinks(id, backlinks)
     
+    cur.close()
     return jsonify({"id": id, "title": title, "body": body})
 
 
--- zettelkasten-front/src/App.js
+++ zettelkasten-front/src/App.js
@@ -1,5 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
     const [cards, setCards] = useState([]);
@@ -15,6 +16,38 @@ function App() {
 	  
     }, []);
 
+    function getCard(id) {
+	// Assuming your backend is running on the same IP and port as in previous example
+	let encoded = encodeURIComponent(id)
+	console.log(encoded)
+	const url = `http://192.168.0.72:5000/cards/${encoded}`;
+	console.log(url)
+
+	// Send a GET request to the URL
+	return fetch(url)
+	    .then(response => {
+		// Check if the response is successful (status code in the range 200-299)
+		if (response.ok) {
+		    // Parse and return the JSON response
+		    return response.json();
+		          
+		} else {
+		    // Throw an error if the response is not successful
+		    throw new Error('Failed to fetch card');
+		          
+		}
+		    
+	    })
+	    .then(cardData => {
+		// Process the card data here (if needed) and return it
+		setViewingCard(cardData);
+		return cardData;
+		    
+	    });
+	
+    }
+    
+    
     function handleSearch(e) {
 	setSearch(e.target.value);
 	  
@@ -32,6 +65,22 @@ function App() {
 	setEditingCard(null);
 	  
     }
+    // ...
+
+    async function handleViewBacklink(backlink) {
+	// Assuming backlink is an object with id and title, you can just use the id to view the card.
+	const cardData = await getCard(backlink.target_id)
+	handleViewCard(cardData);
+    }
+    function handleSidebarCardClick(card) {
+	// Call getCard with the card's id and then call handleViewCard with the fetched cardData
+	getCard(card.id)
+            .then(cardData => {
+		handleViewCard(cardData);
+            });
+    }
+
+
 
     function handleEditCard() {
 	setEditingCard(viewingCard);
@@ -42,7 +91,7 @@ function App() {
     function handleSaveCard() {
 	const url = newCard ?
 	      `http://192.168.0.72:5000/cards`:
-	`http://192.168.0.72:5000/cards/${editingCard.id}` ;
+	      `http://192.168.0.72:5000/cards/${encodeURIComponent(editingCard.id)}` ;
 	const method = newCard ? 'POST' : 'PUT';
 
 	console.log([newCard, url, method])
@@ -69,68 +118,156 @@ function App() {
 	    });
 	
     }
+
+    function renderCardText(body) {
+	const parts = body.split(/(\[[A-Za-z0-9_.-]+\])/); // split the text by bracketed words
+	return parts.map((part, i) => {
+	    // If part is a bracketed word, render a link
+	    if (part.startsWith("[") && part.endsWith("]")) {
+		const cardId = part.substring(1, part.length - 1); // remove brackets to get cardId
+		return (
+		    <a
+			key={i}
+			href="#"
+			onClick={(e) => {
+			    e.preventDefault();
+			    // call the function to handle viewing the card, passing the cardId
+			    handleViewBacklink({"target_id": cardId});
+			}}
+			style={{ color: 'blue' }}
+		    >
+			{part}
+		    </a>
+		);
+	    }
+	    // Otherwise, just render the text
+	    return part;
+	});
+    }
     
-    const filteredCards = cards.filter(
+    const mainCards = cards.filter(card => !card.id.includes('/'));
+    const filteredCards = mainCards.filter(
 	card => card.id.includes(search) || card.title.includes(search)
 	  
     );
 
     return (
 	<div>
-	    <div style={{ width: '20%', float: 'left', borderRight: '1px solid #ccc' }}>
+	    <div className="sidebar" style={{ width: '20%', float: 'left', borderRight: '1px solid #ccc', overflowY: 'auto' }}>
 		<button onClick={handleNewCard}>New Card</button>
 		<input type="text" value={search} onChange={handleSearch} placeholder="Search" />
 		<div>
 		    {filteredCards.map(card => (
-			<div key={card.id} onClick={() => handleViewCard(card)}>
-			    {card.id}: {card.title}
+			<div key={card.id} onClick={() => handleSidebarCardClick(card)}>
+
+
+					<span style={{ color: 'blue', fontWeight: 'bold' }}>
+					    {card.id}
+					</span>			    
+			    : {card.title}
 			</div>
 			          
 		    ))}
 		</div>
 	    </div>
-	    <div style={{ width: '80%', float: 'left', padding: '20px' }}>
+	    <div className="main-content" style={{ width: '80%', float: 'left', padding: '20px', height: '100vh' }}>
 		{viewingCard && (
 		    <div>
-			<h2>{viewingCard.id}</h2>
-			<h3>{viewingCard.title}</h3>
-			<p>{viewingCard.body}</p>
+			<h2 style={{ marginBottom: '10px' }}>
+			    {viewingCard.id}  - {viewingCard.title}
+			</h2>
+			<div style={{ marginBottom: '10px' }}>
+			    {renderCardText(viewingCard.body)}
+			</div>
 			<h4>Backlinks:</h4>
 			<ul>
-			    {viewingCard.backlinks.map((link, index) => (
-				<li key={index}>{link}</li>
-				              
+			    {console.log(viewingCard)}
+			    {viewingCard.backlinks.map((backlink, index) => (
+				<li key={index} style={{ marginBottom: '10px' }}>
+				    <a
+				        href="#"
+					onClick={(e) => {
+					    e.preventDefault();
+					    handleViewBacklink(backlink);
+					                            
+					}}
+					style={{ color: 'black', textDecoration: 'none' }}
+				    >
+					<span style={{ color: 'blue', fontWeight: 'bold' }}>
+					    {backlink.target_id}
+					</span>: {backlink.title}
+				    </a>
+				</li>
+				            
 			    ))}
 			</ul>
 			<button onClick={handleEditCard}>Edit</button>
+			 <h4>Children:</h4>
+			<ul>
+			    {cards.filter(card => card.id.startsWith(`${viewingCard.id}/`)).map((childCard, index) => (
+				<li key={index} style={{ marginBottom: '10px' }}>
+				    <a
+					href="#"
+					onClick={(e) => {
+					    e.preventDefault();
+					    handleViewCard(childCard);
+					}}
+					style={{ color: 'black', textDecoration: 'none' }}
+				    >
+					<span style={{ color: 'blue', fontWeight: 'bold' }}>
+					    {childCard.id}
+					</span>: {childCard.title}
+				    </a>
+				</li>
+			    ))}
+			</ul>
 		    </div>
-		            
+		    
 		)}
+
+
 		{editingCard && (
 		    <div>
+			{newCard ? (
+			// If it's a new card, render an input field for the ID
+			    <input
+				type="text"
+				value={editingCard.id}
+				onChange={e => setEditingCard({ ...editingCard, id: e.target.value })}
+				placeholder="ID"
+				style={{ display: 'block', marginBottom: '10px' }} // Added styles here
+			    />
+			) : (
+			    // If it's an existing card, just display the ID
+			    <div style={{ marginBottom: '10px' }}>ID: {editingCard.id}</div> // Added styles here
+			)}
+			{/* Title Section */}
+			<label htmlFor="title">Title:</label>
 			<input
+			    style={{ display: 'block', width: '100%', marginBottom: '10px' }} // Updated style here
 			    type="text"
-			    value={editingCard.id}
-			    onChange={e => setEditingCard({ ...editingCard, id: e.target.value }
-							 )}
-			    placeholder="ID"
-			/>
-			<input
-			    type="text"
+			    id="title"
 			    value={editingCard.title}
-			    onChange={e => setEditingCard({ ...editingCard, title: e.target.value }
-							 )
-				     }
+			    onChange={e => setEditingCard({ ...editingCard, title: e.target.value })}
 			    placeholder="Title"
 			/>
+			
+			{/* Body Section */}
+			<label htmlFor="body">Body:</label>
 			<textarea
+			    style={{ display: 'block', width: '100%', height: '200px' }} // Updated style here
+			    id="body"
 			    value={editingCard.body}
 			    onChange={e => setEditingCard({ ...editingCard, body: e.target.value })}
 			    placeholder="Body"
 			/>
+			
 			<button onClick={handleSaveCard}>Save</button>
 		    </div>
 		)}
+
+
+		
 	    </div>
 	</div>
     );
