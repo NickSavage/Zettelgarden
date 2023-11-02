07de288a25e6680476a317b53e8de1297f9694cc
Nick Savage <nick@fortuna>
2023-11-02 07:21:26 -0400

    Refactor api

---
+++ zettelkasten-front/src/api.js
@@ -0,0 +1,36 @@
+
+// API
+const base_url = process.env.REACT_APP_URL;
+const username = process.env.REACT_APP_USERNAME;
+const password = process.env.REACT_APP_PASSWORD;
+const creds = btoa(`${username}:${password}`);
+
+export function fetchCards() {
+    return fetch(base_url + '/cards', {headers: {"Authorization": `Basic ${creds}`}})
+	.then(response => response.json())
+}
+
+export function getCard(id) {
+    // Assuming your backend is running on the same IP and port as in previous example
+    let encoded = encodeURIComponent(id)
+    const url = base_url + `/cards/${encoded}`;
+    
+    // Send a GET request to the URL
+    return fetch(url, {headers: {"Authorization": `Basic ${creds}`}})
+	.then(response => {
+	    // Check if the response is successful (status code in the range 200-299)
+	    if (response.ok) {
+		// Parse and return the JSON response
+		return response.json();
+		
+	    } else {
+		// Throw an error if the response is not successful
+		throw new Error('Failed to fetch card');
+	    }
+	})
+	.then(cardData => {
+	    // Process the card data here (if needed) and return it
+	    return cardData;
+	    
+	});
+}
