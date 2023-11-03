828bbfe2eaa8e1ebd84090ce88d71330e4d6524a
Nick Savage <nick@fortuna>
2023-11-02 21:16:36 -0400

    Refactoring

--- zettelkasten-front/src/App.js
+++ zettelkasten-front/src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import {fetchCards, getCard, saveCard} from './api';
 import {getIdByCardId, isCardIdUnique} from './utils';
+import {CardBody} from './components/CardBody';
 
 function App() {
     const [error, setError] = useState("");
@@ -196,43 +197,6 @@ function App() {
     }
 
 
-    function renderCardText(card) {
-	const body = card.body;
-	const parts = body.split(/(\[[A-Za-z0-9_.-/]+\])|(\n)/);
-	return parts.map((part, i) => {
-	    // If part is a new line character, return a break element
-	    if (part === "\n") {
-		return <br key={i} />;
-	    }
-	    // If part is a bracketed word, render a link
-	    else if (part && part.startsWith("[") && part.endsWith("]")) {
-		const cardId = part.substring(1, part.length - 1);
-		const id = getIdByCardId(cards, cardId);
-		
-		const linkedCard = card.direct_links.find(linked => linked.card_id === cardId);
-		const title = linkedCard ? linkedCard.title : "Card not found";
-		return (
-		        <a
-		    key={i}
-		    title={title}
-		    href="#"
-		    onClick={(e) => {
-			e.preventDefault();
-				handleViewBacklink({"id": id, "card_id": cardId
-					   });
-		    }}
-		    style={{ fontWeight: 'bold', color: 'blue' }}
-		        >
-			    {part
-			    }
-			</a>
-		);
-	    }
-	    // Otherwise, just render the text
-	    return part;
-	});
-    }
-
     // Render the warning label
     const renderWarningLabel = () => {
 	if (!editingCard.card_id) return null;
@@ -353,7 +317,7 @@ function App() {
 			</h2>
 			<hr />
 			<div style={{ marginBottom: '10px' }}>
-			    {renderCardText(viewingCard)}
+			    {CardBody(viewingCard, cards, handleViewBacklink)}
 			</div>
 			<div>
 			    {viewingCard.is_reference && <>
---
+++ zettelkasten-front/src/components/CardBody.js
@@ -0,0 +1,47 @@
+import Markdown from 'react-markdown';
+import {getIdByCardId} from '../utils';
+
+function renderCardText(card, cards, handleViewBacklink) {
+    const body = card.body;
+    const parts = body.split(/(\[[A-Za-z0-9_.-/]+\])|(\n)/);
+    return parts.map((part, i) => {
+	// If part is a new line character, return a break element
+	if (part === "\n") {
+	    return <br key={i} />;
+	}
+	// If part is a bracketed word, render a link
+	else if (part && part.startsWith("[") && part.endsWith("]")) {
+	    const cardId = part.substring(1, part.length - 1);
+	    const id = getIdByCardId(cards, cardId);
+	    
+	    const linkedCard = card.direct_links.find(linked => linked.card_id === cardId);
+	    const title = linkedCard ? linkedCard.title : "Card not found";
+	    return (
+		    <a
+		key={i}
+		title={title}
+		href="#"
+		onClick={(e) => {
+		    e.preventDefault();
+		    handleViewBacklink({"id": id, "card_id": cardId
+				       });
+		}}
+		style={{ fontWeight: 'bold', color: 'blue' }}
+		    >
+		    {part
+		    }
+		</a>
+	    );
+	}
+	// Otherwise, just render the text
+	return part;
+    });
+}
+
+export function CardBody(viewingCard, cards, handleViewBacklink) {
+    return (
+	    <div>
+	    {renderCardText(viewingCard, cards, handleViewBacklink)}
+	</div>
+    )	
+}
