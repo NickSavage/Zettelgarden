71bd12b7f5ab47452346ca9ece8d5f8240899abb
Nick Savage <nick@fortuna>
2023-11-04 07:30:56 -0400

    Refactoring to create ViewingPage

--- zettelkasten-front/src/App.js
+++ zettelkasten-front/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import {fetchCards, getCard, saveCard} from './api';
 import {getIdByCardId, isCardIdUnique} from './utils';
 import {CardBody} from './components/CardBody';
+import {ViewingPage} from './components/ViewingPage';
 
 function App() {
     const [error, setError] = useState("");
@@ -306,105 +307,16 @@ function App() {
 		    </div>
 		)}
 		{viewingCard && (
-		    <div>
-			<h2 style={{ marginBottom: '10px' }}>
-			    <span style={{ fontWeight: 'bold', color: 'blue' }}>
-				{viewingCard.card_id} 
-			    </span>
-			    <span>
-				: {viewingCard.title}
-			    </span>
-			</h2>
-			<hr />
-			<div style={{ marginBottom: '10px' }}>
-			    {CardBody(viewingCard, cards, handleViewBacklink)}
-			</div>
-			<div>
-			    {viewingCard.is_reference && <>
-				<span style={{ fontWeight: 'bold' }}>
-				    Link: 
-				</span>
-				<span>
-				    {viewingCard.link}
-				</span>
-			    </>}
-			</div>
-			<hr />
-			<p>
-			    Created At: {viewingCard.created_at}
-			</p>
-			<p>
-			    Updated At: {viewingCard.updated_at}
-			</p>
-			<hr />
-			{parentCard && (
-			    <div>
-			    <h4>Parent:</h4>
-			    <ul>
-				<li style={{ marginBottom: '10px' }}>
-				    <a
-					href="#"
-					onClick={(e) => {
-					    e.preventDefault();
-					    handleViewCard(parentCard);
-					}}
-					style={{ color: 'black', textDecoration: 'none' }}
-				    >
-					<span style={{ color: 'blue', fontWeight: 'bold' }}>
-					    {parentCard.card_id}
-					</span>: {parentCard.title}
-				    </a>
-				</li>
-			    </ul>
-			    </div>
-			)}
-			<h4>Backlinks:</h4>
-			<ul>
-			    {viewingCard.backlinks.map((backlink, index) => (
-				<li key={index} style={{ marginBottom: '10px' }}>
-				    <a
-				        href="#"
-					onClick={(e) => {
-					    e.preventDefault();
-					    handleViewBacklink(backlink);
-					                            
-					}}
-					style={{ color: 'black', textDecoration: 'none' }}
-				    >
-					<span style={{ color: 'blue', fontWeight: 'bold' }}>
-					    {backlink.card_id}
-					</span>: {backlink.title}
-				    </a>
-				</li>
-				            
-			    ))}
-			</ul>
-			<button onClick={handleEditCard}>Edit</button>
-			 <h4>Children:</h4>
-			<ul>
-			    {cards.filter(card => card.card_id.startsWith(`${viewingCard.card_id}/`) || card.card_id.startsWith(`${viewingCard.card_id}.`))
-			     .sort((a, b) => a.card_id.localeCompare(b.card_id))
-			     .map((childCard, index) => (
-				<li key={index} style={{ marginBottom: '10px' }}>
-				    <a
-					href="#"
-					onClick={(e) => {
-					    e.preventDefault();
-					    handleViewCard(childCard);
-					}}
-					style={{ color: 'black', textDecoration: 'none' }}
-				    >
-					<span style={{ color: 'blue', fontWeight: 'bold' }}>
-					    {childCard.card_id}
-					</span>: {childCard.title}
-				    </a>
-				</li>
-			    ))}
-			</ul>
-		    </div>
-		    
-		)}
 
+		    <ViewingPage
+			viewingCard={viewingCard}
+			cards={cards}
+			handleViewBacklink={handleViewBacklink}
+			parentCard={parentCard}
+			handleViewCard={handleViewCard}
+			handleEditCard={handleEditCard}
+		    />
+		)}
 
 		{editingCard && (
 		    <div>
---
+++ zettelkasten-front/src/components/ViewingPage.js
@@ -0,0 +1,103 @@
+import {CardBody} from './CardBody';
+
+export function ViewingPage({viewingCard, cards, handleViewBacklink, parentCard, handleViewCard, handleEditCard}) {
+    return (
+	    <div>
+	    <h2 style={{ marginBottom: '10px' }}>
+	    <span style={{ fontWeight: 'bold', color: 'blue' }}>
+	    {viewingCard.card_id} 
+	</span>
+	    <span>
+	    : {viewingCard.title}
+	</span>
+	    </h2>
+	    <hr />
+	    <div style={{ marginBottom: '10px' }}>
+	    {CardBody(viewingCard, cards, handleViewBacklink)}
+	</div>
+	    <div>
+	    {viewingCard.is_reference && <>
+	     <span style={{ fontWeight: 'bold' }}>
+	     Link: 
+	     </span>
+	     <span>
+	     {viewingCard.link}
+	     </span>
+	     </>}
+	</div>
+	    <hr />
+	    <p>
+	    Created At: {viewingCard.created_at}
+	</p>
+	    <p>
+	    Updated At: {viewingCard.updated_at}
+	</p>
+	    <hr />
+	    {parentCard && (
+		    <div>
+		    <h4>Parent:</h4>
+		    <ul>
+		    <li style={{ marginBottom: '10px' }}>
+		    <a
+		href="#"
+		onClick={(e) => {
+		    e.preventDefault();
+		    handleViewCard(parentCard);
+		}}
+		style={{ color: 'black', textDecoration: 'none' }}
+		    >
+		    <span style={{ color: 'blue', fontWeight: 'bold' }}>
+		    {parentCard.card_id}
+		</span>: {parentCard.title}
+		</a>
+		    </li>
+		    </ul>
+		    </div>
+	    )}
+	    <h4>Backlinks:</h4>
+	    <ul>
+	    {viewingCard.backlinks.map((backlink, index) => (
+		    <li key={index} style={{ marginBottom: '10px' }}>
+		    <a
+		href="#"
+		onClick={(e) => {
+		    e.preventDefault();
+		    handleViewBacklink(backlink);
+		    
+		}}
+		style={{ color: 'black', textDecoration: 'none' }}
+		    >
+		    <span style={{ color: 'blue', fontWeight: 'bold' }}>
+		    {backlink.card_id}
+		</span>: {backlink.title}
+		</a>
+		    </li>
+		    
+	    ))}
+	</ul>
+	    <button onClick={handleEditCard}>Edit</button>
+	    <h4>Children:</h4>
+	    <ul>
+	    {cards.filter(card => card.card_id.startsWith(`${viewingCard.card_id}/`) || card.card_id.startsWith(`${viewingCard.card_id}.`))
+	     .sort((a, b) => a.card_id.localeCompare(b.card_id))
+	     .map((childCard, index) => (
+		     <li key={index} style={{ marginBottom: '10px' }}>
+		     <a
+		 href="#"
+		 onClick={(e) => {
+		     e.preventDefault();
+		     handleViewCard(childCard);
+		 }}
+		 style={{ color: 'black', textDecoration: 'none' }}
+		     >
+		     <span style={{ color: 'blue', fontWeight: 'bold' }}>
+		     {childCard.card_id}
+		 </span>: {childCard.title}
+		 </a>
+		     </li>
+	     ))}
+	</ul>
+	    </div>
+	    
+    )
+}
