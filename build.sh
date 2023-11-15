ecc96cb30fb43aee3c1c4661420bbf9d823e2104
Nick Savage <nick@fortuna>
2023-11-14 20:13:55 -0500

    Add deploy script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source .env
+sudo docker-compose build
+sudo docker-compose push
+
+ssh root@192.168.0.129 <<EOF
+    cd /root/docker
+    docker-compose -f zettel.yml pull
+    docker-compose -f zettel.yml up -d
+EOF
