a4436af4244256c1d72bdeaeba85588634082182
Nick Savage <nick@fortuna>
2023-11-21 06:36:44 -0500

    Improve search

--- backend/app.py
+++ backend/app.py
@@ -122,8 +122,26 @@ def login():
 full_card_query = "SELECT id, card_id, title, body, link, created_at, updated_at FROM cards"
 partial_card_query = "SELECT id, card_id, title FROM cards"
 
-def full_card_query_filtered(search_term) -> str:
-    return full_card_query + " WHERE title ILIKE '%" + search_term + "%' OR body ILIKE '%" + search_term +"%';"
+def full_card_query_filtered(search_terms) -> str:
+    # Split the search string into separate terms
+    terms = search_terms.split()
+
+    # Create a list to hold individual SQL conditions
+    conditions = []
+
+    # For each term, add conditions for both title and body
+    for term in terms:
+        term_condition = f"(title ILIKE '%{term}%' OR body ILIKE '%{term}%')"
+        conditions.append(term_condition)
+
+    # Join the conditions using the AND or OR operator
+    # Use 'AND' for more restrictive searches, 'OR' for broader searches
+    query_condition = ' AND '.join(conditions)
+
+    # Construct the final query
+    final_query = f"{full_card_query} WHERE {query_condition};"
+
+    return final_query
 def partial_card_query_filtered(search_term) -> str:
     return partial_card_query + " WHERE title ILIKE '%" + search_term + "%';"
 
