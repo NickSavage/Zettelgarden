2db375389fcd4df0f731dbc32778ca9b5892b636
Nick Savage <nick@nicksavage.ca>
2023-12-13 20:56:45 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ source .env
 sudo docker-compose build
 sudo docker-compose push
 
-ssh root@192.168.0.129 <<EOF
+ssh root@192.168.0.162 <<EOF
     cd /root/docker
     docker-compose -f zettel.yml pull
     docker-compose -f zettel.yml up -d
