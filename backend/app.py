1a4d530f8556ee11707f9b2825a66e403d001e81
Nick Savage <nick@artemis.lan>
2024-01-04 07:13:49 -0500

    Refactoring

--- backend/app.py
+++ backend/app.py
@@ -22,11 +22,6 @@ def create_app(test_config=None):
     jwt = JWTManager(app)
     bcrypt = Bcrypt(app)  # app is your Flask app instance
 
-    # Database setup
-    DB_FILE = "zettelkasten.db"
-    print('start', flush=True)
-    print([os.getenv('DB_NAME'),os.getenv('DB_USER'),os.getenv('DB_PASS'),os.getenv('DB_HOST'), os.getenv('DB_PORT')], flush=True)
-
     app.register_blueprint(bp)
 
     @app.before_request
@@ -42,115 +37,6 @@ def create_app(test_config=None):
             db.close()
 
     return app
-
-    
-conn = psycopg2.connect(
-    dbname=os.getenv('DB_NAME'),
-    user=os.getenv('DB_USER'),
-    password=os.getenv('DB_PASS'),
-    host=os.getenv('DB_HOST'),
-    port=os.getenv('DB_PORT'),
-    options='-c client_encoding=UTF8'
-)
-
-cur = conn.cursor()
-cur.execute(
-        """
-            CREATE TABLE IF NOT EXISTS cards (
-                id SERIAL PRIMARY KEY,
-                card_id TEXT,
-                user_id INT,
-                title TEXT,
-                body TEXT,
-                link TEXT,
-                created_at TIMESTAMP,
-                updated_at TIMESTAMP,
-                FOREIGN KEY (user_id) REFERENCES users(id)
-        );
-            """
-    )
-cur.execute(
-        """
-            CREATE TABLE IF NOT EXISTS backlinks (
-                source_id TEXT,
-                target_id TEXT,
-                created_at TIMESTAMP,
-                updated_at TIMESTAMP
-            
-        );
-            """
-    )
-cur.execute(
-        """
-            CREATE TABLE IF NOT EXISTS users (
-                id SERIAL PRIMARY KEY,
-                username TEXT,
-                email TEXT,
-                password TEXT,
-                created_at TIMESTAMP,
-                updated_at TIMESTAMP
-        );
-            """
-    )
-cur.execute(
-    """
-        CREATE TABLE IF NOT EXISTS card_views (
-            id SERIAL PRIMARY KEY,
-            card_pk INT, 
-            user_id INT,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY (card_pk) REFERENCES cards(id),
-            FOREIGN KEY (user_id) REFERENCES users(id)
-    )
-    """
-)
-cur.execute(
-    """
-        CREATE TABLE IF NOT EXISTS categories (
-            id SERIAL PRIMARY KEY,
-            user_id INT,
-            name TEXT,
-            description TEXT,
-            regex TEXT,
-            is_active BOOLEAN DEFAULT TRUE,
-            created_by INT,
-            updated_by INT,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY (user_id) REFERENCES users(id),
-            FOREIGN KEY (created_by) REFERENCES users(id),
-            FOREIGN KEY (updated_by) REFERENCES users(id)
-    )
-    """
-)
-cur.execute(
-    """
-    CREATE TABLE IF NOT EXISTS files (
-        id SERIAL PRIMARY KEY,
-        name TEXT,
-        type TEXT,
-        path TEXT,
-        filename TEXT,
-        size INT,
-        created_by INT,
-        updated_by INT,
-        card_pk INT, 
-        is_deleted BOOLEAN DEFAULT FALSE,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (created_by) REFERENCES users(id),
-        FOREIGN KEY (updated_by) REFERENCES users(id),
-        FOREIGN KEY (card_pk) REFERENCES cards(id)
-    )
-    """
-)
-conn.commit()
-cur.close()
-
-# login
-    
-# Serializers
-
 
 
 if __name__ == "__main__":
--- backend/database.py
+++ backend/database.py
@@ -18,3 +18,99 @@ def get_db():
     if 'db' not in g:
         g.db = connect_to_database()
     return g.db
+
+conn = connect_to_database()
+
+cur = conn.cursor()
+cur.execute(
+        """
+            CREATE TABLE IF NOT EXISTS cards (
+                id SERIAL PRIMARY KEY,
+                card_id TEXT,
+                user_id INT,
+                title TEXT,
+                body TEXT,
+                link TEXT,
+                created_at TIMESTAMP,
+                updated_at TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users(id)
+        );
+            """
+    )
+cur.execute(
+        """
+            CREATE TABLE IF NOT EXISTS backlinks (
+                source_id TEXT,
+                target_id TEXT,
+                created_at TIMESTAMP,
+                updated_at TIMESTAMP
+            
+        );
+            """
+    )
+cur.execute(
+        """
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                username TEXT,
+                email TEXT,
+                password TEXT,
+                created_at TIMESTAMP,
+                updated_at TIMESTAMP
+        );
+            """
+    )
+cur.execute(
+    """
+        CREATE TABLE IF NOT EXISTS card_views (
+            id SERIAL PRIMARY KEY,
+            card_pk INT, 
+            user_id INT,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (card_pk) REFERENCES cards(id),
+            FOREIGN KEY (user_id) REFERENCES users(id)
+    )
+    """
+)
+cur.execute(
+    """
+        CREATE TABLE IF NOT EXISTS categories (
+            id SERIAL PRIMARY KEY,
+            user_id INT,
+            name TEXT,
+            description TEXT,
+            regex TEXT,
+            is_active BOOLEAN DEFAULT TRUE,
+            created_by INT,
+            updated_by INT,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (user_id) REFERENCES users(id),
+            FOREIGN KEY (created_by) REFERENCES users(id),
+            FOREIGN KEY (updated_by) REFERENCES users(id)
+    )
+    """
+)
+cur.execute(
+    """
+    CREATE TABLE IF NOT EXISTS files (
+        id SERIAL PRIMARY KEY,
+        name TEXT,
+        type TEXT,
+        path TEXT,
+        filename TEXT,
+        size INT,
+        created_by INT,
+        updated_by INT,
+        card_pk INT, 
+        is_deleted BOOLEAN DEFAULT FALSE,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (created_by) REFERENCES users(id),
+        FOREIGN KEY (updated_by) REFERENCES users(id),
+        FOREIGN KEY (card_pk) REFERENCES cards(id)
+    )
+    """
+)
+conn.commit()
+cur.close()
