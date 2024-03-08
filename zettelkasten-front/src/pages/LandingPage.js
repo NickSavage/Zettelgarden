49c4b0b01146dd70c17d085724453809a8b61cb9
Nick Savage <nick@nicksavage.ca>
2024-03-08 08:57:07 -0500

    Add redirect from landing page to app

--- zettelkasten-front/src/pages/LandingPage.js
+++ zettelkasten-front/src/pages/LandingPage.js
@@ -1,8 +1,14 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function LandingPage() {
-    return (
-	<div>test</div>
-    );
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate('/app');
+    }, [navigate]); // Dependency array to ensure the effect runs once
+
+    return <div>Redirecting...</div>;
 }
 
 export default LandingPage;
