108321513d12ecd17f214b5543dbc0b39d59dd2f
Nick Savage <nick@nicksavage.ca>
2024-04-09 21:13:48 -0400

    change CardItem to be actual links instead of fake links.

--- zettelkasten-front/src/components/CardItem.js
+++ zettelkasten-front/src/components/CardItem.js
@@ -1,15 +1,15 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 export function CardItem({ handleViewCard, card }) {
   const navigate = useNavigate();
-  function handleCardClick(card_id) {
-    navigate(`/app/card/${card_id}`);
-  }
   return (
-    <div key={card.id} onClick={() => handleCardClick(card.id)}>
+    <div key={card.id}>
+      <Link to={`/app/card/${card.id}`} style={{ textDecoration: 'none', color: 'inherit'}}>
         <span style={{ color: "blue", fontWeight: "bold" }}>
          - {card.card_id}
         </span>
         : {card.title}
+        </Link>
       </div>
   );
 }
