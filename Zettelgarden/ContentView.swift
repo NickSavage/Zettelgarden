db56a23e8fd2a40838f644600ad5e9614d47151d
Nick Savage <nick@nicksavage.ca>
2024-05-13 17:19:33 -0400

    Initial Commit

---
+++ Zettelgarden/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zettelgarden
+//
+//  Created by Nicholas Savage on 2024-05-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zettelgarden/ZettelgardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZettelgardenApp.swift
+//  Zettelgarden
+//
+//  Created by Nicholas Savage on 2024-05-13.
+//
+
+import SwiftUI
+
+@main
+struct ZettelgardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
