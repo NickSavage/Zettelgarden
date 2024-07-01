ffeeb73415fe0f05b2b0b0aace2de23b00fa5818
Nick Savage <nick@nicksavage.ca>
2024-07-01 06:43:03 -0400

    implement CardPreviewWindow on CardItem

--- zettelkasten-front/src/components/CardItem.tsx
+++ zettelkasten-front/src/components/CardItem.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { PartialCard } from "../models/Card";
+
+import { CardPreviewWindow } from "./CardPreviewWindow";
 
 interface CardItemProps {
   card: PartialCard;
 }
 
 export function CardItem({ card }: CardItemProps) {
+  const [showHover, setShowHover] = useState(false);
   return (
-    <div key={card.id}>
+    <div key={card.id}
+      onMouseEnter={() => setShowHover(true)}
+      onMouseLeave={() => setShowHover(false)}
+    >
       <Link
         to={`/app/card/${card.id}`}
         style={{ textDecoration: "none", color: "inherit" }}
@@ -18,6 +24,7 @@ export function CardItem({ card }: CardItemProps) {
         </span>
         : {card.title}
       </Link>
+      {showHover && card && <CardPreviewWindow card={card} />}
     </div>
   );
 }
