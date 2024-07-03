5a66726dd1811e093739245c5018eb24d81a6e5b
Nick Savage <nick@nicksavage.ca>
2024-07-03 15:18:06 -0400

    working on converting to next.js

---
+++ zettelkasten-front/src/app/[[...slug]]/client.tsx
@@ -0,0 +1,20 @@
+'use client'
+ 
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from 'src/AuthContext'
+import dynamic from 'next/dynamic'
+ 
+const App = dynamic(() => import('../../App'), { ssr: false })
+ 
+export function ClientOnly() {
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </React.StrictMode>
+  )
+}
---
+++ zettelkasten-front/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import '../App.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: "Zettelgarden",
+    description: "Website"
+
+}
+
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+<html lang="en">
+  <body>
+    <div id="root">{children}</div>
+  </body>
+</html>
+
+    )    
+  }
