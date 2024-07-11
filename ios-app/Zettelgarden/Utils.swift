dc345f9fddad50d90d4b67726541d055f4694a18
Nick Savage <nick@nicksavage.ca>
2024-07-11 19:57:34 -0400

    working on date parsing for cards

--- ios-app/Zettelgarden/Utils.swift
+++ ios-app/Zettelgarden/Utils.swift
@@ -136,6 +136,7 @@ func cardToPartialCard(card: Card) -> PartialCard {
     return PartialCard(
         id: card.id,
         card_id: card.card_id,
+        user_id: card.user_id,
         title: card.title,
         created_at: card.created_at,
         updated_at: card.updated_at
