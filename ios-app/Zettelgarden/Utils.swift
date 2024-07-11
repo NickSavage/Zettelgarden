9650a5373b3e4c59f22f1b4e52d3d2aa641cf499
Nick Savage <nick@nicksavage.ca>
2024-07-11 17:51:01 -0400

    formatting

--- ios-app/Zettelgarden/Utils.swift
+++ ios-app/Zettelgarden/Utils.swift
@@ -30,7 +30,9 @@ func performRequest<T: Decodable>(
             return
         }
 
-        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 || httpResponse.statusCode == 201 else {
+        guard let httpResponse = response as? HTTPURLResponse,
+            httpResponse.statusCode == 200 || httpResponse.statusCode == 201
+        else {
             let statusError = NSError(
                 domain: "",
                 code: (response as? HTTPURLResponse)?.statusCode ?? 500,
