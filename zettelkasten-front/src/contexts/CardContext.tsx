bfcd75de1372165395974e8c840bc7390ec7ffb8
Nick Savage <nick@nicksavage.ca>
2024-07-16 06:41:08 -0400

    add cardcontext

---
+++ zettelkasten-front/src/contexts/CardContext.tsx
@@ -0,0 +1,45 @@
+import React, { createContext, useState, useEffect, useContext } from "react";
+import { fetchPartialCards } from "src/api/cards";
+import { PartialCard } from "src/models/Card";
+
+
+interface PartialCardContextType {
+    partialCards: PartialCard[];
+    refreshPartialCards: boolean;
+    setRefreshPartialCards: (refrsh: boolean) => void;
+    getPartialCards: () => Promise<void>;
+}
+const PartialCardContext = createContext<PartialCardContextType | undefined>(undefined);
+
+export const PartialCardProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const [partialCards, setPartialCards] = useState<PartialCard[]>([]);
+    const [refreshPartialCards, setRefreshPartialCards] = useState<boolean>(false);
+
+    const getPartialCards = async () => {
+        console.log("hmm?")
+        await fetchPartialCards("", "date").then((data) => {
+            setPartialCards(data)
+            setRefreshPartialCards(false);
+        })
+    }
+
+    useEffect(() => {
+      if (refreshPartialCards) {
+        getPartialCards();
+      }
+    }, [refreshPartialCards]);
+
+    return (
+        <PartialCardContext.Provider value={{ partialCards, refreshPartialCards, setRefreshPartialCards, getPartialCards }}>
+            {children}
+        </PartialCardContext.Provider>
+    )
+}
+
+export const usePartialCardContext = () => {
+    const context = useContext(PartialCardContext);
+    if (context === undefined) {
+        throw new Error('usePartialCardContext must be used wtihin a PartialCardProvider');
+    }
+    return context;
+}
