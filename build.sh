6172945c6117aaf3fe35a2d4d757b9595f5bcd20
Nick Savage <nick@nicksavage.ca>
2024-07-22 16:07:45 -0400

    deployment changes

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 source .env
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/id_rsa
+
 sudo docker-compose build
 sudo docker-compose push
 
-ssh root@192.168.0.162 <<EOF
+ssh root@zettelgarden <<EOF
     cd /root/docker
-    docker-compose -f zettel.yml pull
-    docker-compose -f zettel.yml up -d
+    docker compose -f zettel.yml pull
+    docker compose -f zettel.yml up -d
 EOF
