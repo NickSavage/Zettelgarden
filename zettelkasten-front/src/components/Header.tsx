44dbc319d602f7d8da8ca73232bbe04fff7dbc35
Nick Savage <nick@nicksavage.ca>
2024-07-23 17:53:03 -0400

    add className to headers

--- zettelkasten-front/src/components/Header.tsx
+++ zettelkasten-front/src/components/Header.tsx
@@ -2,20 +2,21 @@ import React from "react";
 
 interface HeaderProps {
   text: string;
+  className?: string;
 }
 
-export function HeaderTop({ text }: HeaderProps) {
-  return <span className="font-bold text-xl">{text}</span>;
+export function HeaderTop({ text, className }: HeaderProps) {
+  return <span className={`font-bold text-xl ${className || ""}`}>{text}</span>;
 }
 
-export function HeaderSection({ text }: HeaderProps) {
-  return <span className="font-bold text-l">{text}</span>;
+export function HeaderSection({ text, className }: HeaderProps) {
+  return <span className={`font-bold text-l ${className || ""}`}>{text}</span>;
 }
 
-export function HeaderSubSection({ text }: HeaderProps) {
+export function HeaderSubSection({ text, className }: HeaderProps) {
   return (
     <div>
-      <span className="font-bold">{text}</span>
+      <span className={`font-bold ${className || ""}`}>{text}</span>
       <br />
     </div>
   );
