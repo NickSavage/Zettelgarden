9ec59402a26bf108cccecb4b086de0d3b06f9b8a
Nick Savage <nick@nicksavage.ca>
2024-09-06 20:13:17 -0400

    frontend: disable subscriptions for now

--- zettelkasten-front/src/contexts/AuthContext.tsx
+++ zettelkasten-front/src/contexts/AuthContext.tsx
@@ -30,7 +30,7 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
   const [isLoading, setIsLoading] = useState(true); // Added loading state
-  const [hasSubscription, setHasSubscription] = useState<boolean>(false);
+  const [hasSubscription, setHasSubscription] = useState<boolean>(true);
   const [currentUser, setCurrentUser] = useState<User | null>(null);
 
   useEffect(() => {
@@ -45,7 +45,8 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
         const currentUser = await getCurrentUser();
         setCurrentUser(currentUser);
         console.log("user", currentUser)
-        setHasSubscription(currentUser.stripe_subscription_status === "active");
+	// setHasSubscription(currentUser.stripe_subscription_status === "active");
+	setHasSubscription(true);
       }
       setIsLoading(false);
     };
