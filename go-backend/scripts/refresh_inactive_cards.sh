16f4dd0d375970b35737f22e3a8735ad032f5797
Nick Savage <nick@nicksavage.ca>
2024-09-28 22:40:32 -0400

    go-backend: source bash_profile while running inactive script

--- go-backend/scripts/refresh_inactive_cards.sh
+++ go-backend/scripts/refresh_inactive_cards.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source /home/nick/code/projects/zettelkasten/.env-prod
+source /home/nick/.bash_profile
 go run /home/nick/code/projects/zettelkasten/go-backend/scripts/refresh_inactive_cards.go
