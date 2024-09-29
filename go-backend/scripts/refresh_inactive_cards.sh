091dc25c8858e16319eb723cce5ecfa63e35ae39
Nick Savage <nick@nicksavage.ca>
2024-09-28 22:45:20 -0400

    backend: fix inactive script

--- go-backend/scripts/refresh_inactive_cards.sh
+++ go-backend/scripts/refresh_inactive_cards.sh
@@ -2,4 +2,5 @@
 
 source /home/nick/code/projects/zettelkasten/.env-prod
 source /home/nick/.bash_profile
+cd /home/nick/code/projects/zettelkasten/go-backend
 go run /home/nick/code/projects/zettelkasten/go-backend/scripts/refresh_inactive_cards.go
