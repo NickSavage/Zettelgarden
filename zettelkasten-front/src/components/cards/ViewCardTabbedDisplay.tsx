917174ff09b744177880cdbe6bf5b3b5a5bf3eff
Nick Savage <nick@nicksavage.ca>
2024-10-06 07:41:15 -0400

    frontend: move tabbed display to its own file

---
+++ zettelkasten-front/src/components/cards/ViewCardTabbedDisplay.tsx
@@ -0,0 +1,156 @@
+import React, { useState, useEffect } from "react";
+import { Card, PartialCard } from "../../models/Card";
+
+import {
+  HeaderTop,
+  HeaderSection,
+  HeaderSubSection,
+} from "../../components/Header";
+import { compareCardIds } from "../../utils/cards";
+import { isErrorResponse } from "../../models/common";
+
+import { saveExistingCard, fetchRelatedCards } from "../../api/cards";
+import { CardList } from "../../components/cards/CardList";
+
+import { FileListItem } from "../../components/files/FileListItem";
+
+import { ChildrenCards } from "../../components/cards/ChildrenCards";
+
+interface ViewCardTabbedDisplay {
+  viewingCard: Card;
+  setViewCard: (card: Card) => void;
+}
+
+export function ViewCardTabbedDisplay({
+  viewingCard,
+  setViewCard,
+
+}: ViewCardTabbedDisplay) {
+  const [activeTab, setActiveTab] = useState<string>("Children");
+  const [relatedCards, setRelatedCards] = useState<PartialCard[]>([]);
+
+  const tabs = [
+    { label: "Children" },
+    { label: "References" },
+    { label: "Related" },
+    { label: "Files" },
+  ];
+
+
+  function onFileDelete(file_id: number) {}
+  function handleViewCard(card_id: number) {}
+  function openRenameModal(file: File) {}
+
+  function handleTabClick(label: string) {
+    setActiveTab(label);
+  }
+
+  async function handleDisplayFileOnCardClick(file: File) {
+    if (viewingCard === null) {
+      return;
+    }
+
+    let editedCard = {
+      ...viewingCard,
+      body: viewingCard.body + "\n\n![](" + file.id + ")",
+    };
+    let response = await saveExistingCard(editedCard);
+    setViewCard(editedCard);
+    fetchCard(id!);
+  }
+
+  async function handleFetchRelatedCards(id: string) {
+    let response = await fetchRelatedCards(id);
+
+    if (isErrorResponse(response)) {
+      setError(response["error"]);
+    } else {
+      setRelatedCards(response);
+    }
+  }
+
+  useEffect(() => {
+
+    handleFetchRelatedCards(viewingCard.id.toString());
+  }, [viewingCard])
+
+  return (
+    <div>
+      <div className="flex">
+        {tabs.map((tab) => (
+          <span
+            key={tab.label}
+            onClick={() => handleTabClick(tab.label)}
+            className={`
+            cursor-pointer font-medium py-1.5 px-3 rounded-md flex items-center
+            ${
+              activeTab === tab.label
+                ? "text-blue-600 border-b-4 border-blue-600"
+                : "text-gray-600 hover:text-gray-800 hover:bg-gray-100"
+            }
+          `}
+          >
+            {tab.label}
+            <span className="ml-1 text-xs font-semibold bg-gray-200 rounded-full px-2 py-0.5 text-gray-700">
+              {tab.label === "Children" && viewingCard.children.length}
+              {tab.label === "References" && viewingCard.references.length}
+              {tab.label === "Related" && relatedCards.length}
+              {tab.label === "Files" && viewingCard.files.length}
+            </span>
+          </span>
+        ))}
+      </div>
+      {activeTab === "References" && (
+        <div>
+          <HeaderSubSection text="References" />
+          <CardList
+            cards={viewingCard.references.sort((a, b) =>
+              compareCardIds(a.card_id, b.card_id),
+            )}
+          />
+        </div>
+      )}
+      {activeTab === "Children" && (
+        <div>
+          {viewingCard.children.length > 0 && (
+            <div>
+              <HeaderSubSection text="Children" />
+              <ChildrenCards
+                allChildren={viewingCard.children.sort((a, b) =>
+                  compareCardIds(a.card_id, b.card_id),
+                )}
+                card={viewingCard}
+              />
+            </div>
+          )}
+        </div>
+      )}
+      {activeTab === "Related" && (
+        <div>
+          <HeaderSubSection text="Related Cards" />
+          <CardList cards={relatedCards} />
+        </div>
+      )}
+      {activeTab === "Files" && (
+        <div>
+          {viewingCard.files.length > 0 && (
+            <div>
+              <ul>
+                {viewingCard.files.map((file, index) => (
+                  <FileListItem
+                    file={file}
+                    onDelete={onFileDelete}
+                    setRefreshFiles={(refresh: boolean) => {}}
+                    displayFileOnCard={(file: File) => {
+                      handleDisplayFileOnCardClick(file);
+                    }}
+                  />
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
--- zettelkasten-front/src/pages/cards/ViewPage.tsx
+++ zettelkasten-front/src/pages/cards/ViewPage.tsx
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { CardBody } from "../../components/cards/CardBody";
 import { CardItem } from "../../components/cards/CardItem";
-import { CardList } from "../../components/cards/CardList";
-import { FileListItem } from "../../components/files/FileListItem";
 import { BacklinkInput } from "../../components/cards/BacklinkInput";
 import { getCard, saveExistingCard, fetchRelatedCards } from "../../api/cards";
-import { compareCardIds } from "../../utils/cards";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
@@ -23,8 +20,8 @@ import {
 import { Button } from "../../components/Button";
 import { linkifyWithDefaultOptions } from "../../utils/strings";
 import { convertCardToPartialCard } from "../../utils/cards";
-import { ChildrenCards } from "../../components/cards/ChildrenCards";
 import { FileUpload } from "../../components/files/FileUpload";
+import { ViewCardTabbedDisplay } from "../../components/cards/ViewCardTabbedDisplay";
 
 import { SearchTagMenu } from "../../components/tags/SearchTagMenu";
 import { useTagContext } from "../../contexts/TagContext";
@@ -36,7 +33,6 @@ export function ViewPage({}: ViewPageProps) {
   const [error, setError] = useState("");
   const [viewingCard, setViewCard] = useState<Card | null>(null);
   const [parentCard, setParentCard] = useState<Card | null>(null);
-  const [relatedCards, setRelatedCards] = useState<PartialCard[]>([]);
   const { tasks, setRefreshTasks } = useTaskContext();
   const [showCreateTaskWindow, setShowCreateTaskWindow] =
     useState<boolean>(false);
@@ -50,19 +46,6 @@ export function ViewPage({}: ViewPageProps) {
 
   const { setLastCard } = usePartialCardContext();
   const { tags } = useTagContext();
-
-  const [activeTab, setActiveTab] = useState<string>("Children");
-
-  const tabs = [
-    { label: "Children" },
-    { label: "References" },
-    { label: "Related" },
-    { label: "Files" },
-  ];
-
-  function onFileDelete(file_id: number) {}
-  function handleViewCard(card_id: number) {}
-  function openRenameModal(file: File) {}
 
   async function handleAddBacklink(selectedCard: PartialCard) {
     if (viewingCard === null) {
@@ -91,20 +74,6 @@ export function ViewPage({}: ViewPageProps) {
     let editedCard = {
       ...viewingCard,
       body: viewingCard.body + "\n\n#" + tagName,
-    };
-    let response = await saveExistingCard(editedCard);
-    setViewCard(editedCard);
-    fetchCard(id!);
-  }
-
-  async function handleDisplayFileOnCardClick(file: File) {
-    if (viewingCard === null) {
-      return;
-    }
-
-    let editedCard = {
-      ...viewingCard,
-      body: viewingCard.body + "\n\n![](" + file.id + ")",
     };
     let response = await saveExistingCard(editedCard);
     setViewCard(editedCard);
@@ -146,26 +115,11 @@ export function ViewPage({}: ViewPageProps) {
     setShowCreateTaskWindow(!showCreateTaskWindow);
   }
 
-  async function handleFetchRelatedCards(id: string) {
-    let response = await fetchRelatedCards(id);
-
-    if (isErrorResponse(response)) {
-      setError(response["error"]);
-    } else {
-      setRelatedCards(response);
-    }
-  }
-
-  function handleTabClick(label: string) {
-    setActiveTab(label);
-  }
-
   // For initial fetch and when id changes
   useEffect(() => {
     setError("");
     fetchCard(id!);
     setRefreshTasks(true);
-    handleFetchRelatedCards(id!);
   }, [id, setRefreshTasks]);
 
   return (
@@ -260,85 +214,7 @@ export function ViewPage({}: ViewPageProps) {
               onTagClick={(tag: string) => {}}
             />
           ))}
-
-          <div>
-            <div className="flex">
-              {tabs.map((tab) => (
-                <span
-                  key={tab.label}
-                  onClick={() => handleTabClick(tab.label)}
-                  className={`
-            cursor-pointer font-medium py-1.5 px-3 rounded-md flex items-center
-            ${
-              activeTab === tab.label
-                ? "text-blue-600 border-b-4 border-blue-600"
-                : "text-gray-600 hover:text-gray-800 hover:bg-gray-100"
-            }
-          `}
-                >
-                  {tab.label}
-                  <span className="ml-1 text-xs font-semibold bg-gray-200 rounded-full px-2 py-0.5 text-gray-700">
-                    {tab.label === "Children" && viewingCard.children.length}
-                    {tab.label === "References" &&
-                      viewingCard.references.length}
-                    {tab.label === "Related" && relatedCards.length}
-                    {tab.label === "Files" && viewingCard.files.length}
-                  </span>
-                </span>
-              ))}
-            </div>
-            {activeTab === "References" && (
-              <div>
-                <HeaderSubSection text="References" />
-                <CardList
-                  cards={viewingCard.references.sort((a, b) =>
-                    compareCardIds(a.card_id, b.card_id),
-                  )}
-                />
-              </div>
-            )}
-            {activeTab === "Children" && (
-              <div>
-                {viewingCard.children.length > 0 && (
-                  <div>
-                    <HeaderSubSection text="Children" />
-                    <ChildrenCards
-                      allChildren={viewingCard.children.sort((a, b) =>
-                        compareCardIds(a.card_id, b.card_id),
-                      )}
-                      card={viewingCard}
-                    />
-                  </div>
-                )}
-              </div>
-            )}
-            {activeTab === "Related" && (
-              <div>
-                <HeaderSubSection text="Related Cards" />
-                <CardList cards={relatedCards} />
-              </div>
-            )}
-            {activeTab === "Files" && (
-              <div>
-                {viewingCard.files.length > 0 && (
-                  <div>
-                    <ul>
-                      {viewingCard.files.map((file, index) => (
-                        <FileListItem
-                          file={file}
-                          onDelete={onFileDelete}
-                          setRefreshFiles={(refresh: boolean) => {}}
-                          displayFileOnCard={(file: File) => {
-                            handleDisplayFileOnCardClick(file);
-                          }}
-                        />
-                      ))}
-                    </ul>
-                  </div>
-                )}
-              </div>
-            )}
-          </div>
+	  <ViewCardTabbedDisplay viewingCard={viewingCard} setViewCard={setViewCard} />
         </div>
       )}
     </div>
