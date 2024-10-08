b899f81454b61ab8624bb1536b4d29c4468d246e
Nick Savage <nick@nicksavage.ca>
2024-10-07 20:24:52 -0400

    ios-app: refactroing AddTagMenuView

--- ios-app/Zettelgarden/Views/Tasks/Components/TaskAddTagView.swift
+++ ios-app/Zettelgarden/Views/Tasks/Components/TaskAddTagView.swift
@@ -6,39 +6,15 @@ struct TaskAddTagView: View {
     @StateObject var taskViewModel: TaskViewModel
     @StateObject var taskListViewModel: TaskListViewModel
     @Binding var isPresented: Bool
-    @State private var newTag: String = ""
 
-    private func filteredTags() -> [String] {
-        return taskListViewModel.existingTags.filter { !taskViewModel.tags.contains($0) }
-    }
-    private func addNewTag() {
-        var trimmedTag = newTag.trimmingCharacters(in: .whitespacesAndNewlines)
-
-        if !trimmedTag.hasPrefix("#") {
-            trimmedTag = "#\(trimmedTag)"
-        }
-
-        if !trimmedTag.isEmpty {
-            taskViewModel.addTag(tag: trimmedTag)
-            newTag = ""
-            isPresented = false
-        }
+    private func onSelect(_ tagName: String) {
+        taskViewModel.addTag(tag: tagName)
+        isPresented = false
     }
 
     var body: some View {
         VStack {
-            TextField("Enter new tag", text: $newTag, onCommit: addNewTag)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-            ForEach(filteredTags(), id: \.self) { tag in
-                Button(action: {
-                    taskViewModel.addTag(tag: tag)
-                    isPresented = false
-                }) {
-                    Text(tag)
-                }
-                .padding(.vertical, 2)  // Add some vertical padding between buttons
-            }
+            AddTagMenuView(onSelect: onSelect)
 
         }
     }
