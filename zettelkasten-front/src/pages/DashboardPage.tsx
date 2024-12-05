32fe908dcf8fbe487ef577fa1d3ae17931b42d53
Nick Savage <nick@nicksavage.ca>
2024-12-05 16:11:24 -0500

    frontend: fix styling on dashboard

--- zettelkasten-front/src/pages/DashboardPage.tsx
+++ zettelkasten-front/src/pages/DashboardPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { fetchPartialCards, getCard } from "../api/cards";
+import { sortCards } from "../utils/cards";
 import { useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { PartialCard, Card } from "../models/Card";
@@ -21,48 +22,21 @@ export function DashboardPage() {
   const [refresh, setRefresh] = React.useState<boolean>(false);
   const { tasks, setRefreshTasks } = useTaskContext();
   const [message, setMessage] = React.useState<string>("");
-  const { currentUser } = useAuth();
-
-  const { showCreateTaskWindow, setShowCreateTaskWindow } =
-    useShortcutContext();
 
   const navigate = useNavigate();
 
-  const [displayCard, setDisplayCard] = React.useState<Card | null>(null);
-
-  async function fetchDisplayCard(cardPK: number) {
-    await getCard(cardPK.toString()).then((card) => {
-      setDisplayCard(card);
-    });
-  }
-
-  function handleNewCard() {
-    navigate("/app/card/new", { state: { cardType: "standard" } });
-  }
-
-  function handleClickNewTask() {
-    setShowCreateTaskWindow(true);
-  }
-
-  useEffect(() => {
-    if (currentUser) {
-      if (currentUser.dashboard_card_pk) {
-        fetchDisplayCard(currentUser.dashboard_card_pk);
-      }
-    }
-  }, [currentUser]);
+  const recentCards = sortCards(partialCards.slice(0, 10), "sortOldNew");
 
   return (
     <div>
       {/* Main Content Section */}
       <div className="p-2">
-        <div>
-          <hr />
+        <div className="text-center">
           <div className="mb-8">
-            <h1 className="text-3xl font-bold text-gray-900 mb-3">
+            <h1 className="text-3xl font-bold text-gray-900 mb-3 p-10">
               Welcome to Zettelgarden 🌱
             </h1>
-            <p className="text-lg text-gray-600 max-w-2xl">
+            <p className="text-lg text-gray-600 max-w-full">
               Your personal space for growing ideas. Create cards, connect
               thoughts, and watch your knowledge garden flourish.
             </p>
@@ -74,20 +48,17 @@ export function DashboardPage() {
 
         <div className="flex-grow md:w-8/12 border-r p-4">
           <span className="font-bold">Recent Cards</span>
+          {partialCards && <CardList cards={partialCards.slice(0, 10)} />}
+          <hr />
         </div>
 
         {/* Right Section */}
         <div className="flex-shrink-0 md:w-4/12 border-l p-4">
           <div>
             <span className="font-bold">Unsorted Cards</span>
-            {partialCards && (
-              <CardList
-                cards={partialCards
-                  .filter((card) => card.card_id === "")
-                  .slice(0, 10)}
-              />
-            )}
+            {partialCards && <CardList cards={recentCards} />}
           </div>
+          <hr />
         </div>
       </div>
     </div>
