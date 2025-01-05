60b868b66b99ed5c23fdbfcb5bc67922ef075a84
Nick Savage <nick@nicksavage.ca>
2025-01-04 20:12:10 -0500

    landing: fixing broken link

--- zettelkasten-front/src/landing/LandingPage.tsx
+++ zettelkasten-front/src/landing/LandingPage.tsx
@@ -174,7 +174,7 @@ function LandingPage() {
                 Report an Issue
               </a>
               <a 
-                href="https://github.com/NickSavage/Zettelgarden/blob/main/CONTRIBUTING.md" 
+                href="https://github.com/NickSavage/Zettelgarden/blob/master/CONTRIBUTING.md" 
                 className="hover:text-gray-900 flex items-center">
                 <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z"/>
