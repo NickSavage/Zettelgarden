5a100cc965a6f8e56e8dd14aafffd015be7bb9d9
Nick Savage <nick@nicksavage.ca>
2025-02-04 21:14:21 -0500

    add nginx.conf to build script

--- build.sh
+++ build.sh
@@ -6,8 +6,11 @@ ssh-add ~/.ssh/id_rsa
 sudo docker-compose build
 sudo docker-compose push
 
+# todo: make dirs automatically
 scp docker-zettel-run.yml $SSH_USER@$SSH_HOST:$BUILD_DIR
+scp nginx/nginx.conf $SSH_USER@$SSH_HOST:$BUILD_DIR/nginx/nginx.conf
 ssh $SSH_USER@$SSH_HOST <<EOF
+
     cd $BUILD_DIR
     docker compose -f docker-zettel-run.yml pull
     docker compose -f docker-zettel-run.yml up -d
