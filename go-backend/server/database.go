5d89583dacc8cde32e7324770d4bee2b493b82cb
Nick Savage <nick@nicksavage.ca>
2025-05-06 20:05:30 -0400

    fix backend ci

--- go-backend/server/database.go
+++ go-backend/server/database.go
@@ -54,6 +54,7 @@ func ResetDatabase(S *Server) error {
 			DROP TABLE IF EXISTS user_llm_configurations CASCADE;
 			DROP TABLE IF EXISTS pinned_cards CASCADE;
 			DROP TABLE IF EXISTS card_templates CASCADE;
+			DROP TABLE IF EXISTS pinned_searches CASCADE;
 
 			CREATE TABLE IF NOT EXISTS migrations (
 				id SERIAL PRIMARY KEY,
